bba196c0c19e054680334847ddf36bcc2e339c76
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2023-05-02 04:13:53 +0600

    Prettify

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-const site = window.location.hostname;
-// let a = document.querySelector("nav");
-
-if (site.includes("cirrus-ci.com")) {
+if (window.location.hostname.includes("cirrus-ci.com")) {
   setTimeout(inject, 1000);
 }
 
 function inject() {
-  addBtn("All", "nav", all);
-  addBtn("PR", "nav", onlyPR);
-  addBtn("Branch", "nav", onlyBranch);
-  addBtn("Executing", "nav", onlyExecuting);
-  addBtn("Aborted", "nav", onlyAborted);
-  addBtn("Completed", "nav", onlyCompleted);
-  addBtn("Failed", "nav", onlyFailed);
+  addBtn("All", all);
+  addBtn("PR", onlyPR);
+  addBtn("Branch", onlyBranch);
+  addBtn("Executing", onlyExecuting);
+  addBtn("Aborted", onlyAborted);
+  addBtn("Completed", onlyCompleted);
+  addBtn("Failed", onlyFailed);
 
-  let a = document.querySelector("#btnAll");
-  let b = document.querySelector("#btnPR");
-  let c = document.querySelector("#btnBranch");
-  let d = document.querySelector("#btnExecuting");
-  let e = document.querySelector("#btnAborted");
-  let f = document.querySelector("#btnCompleted");
-  let g = document.querySelector("#btnFailed");
-
-  all.call(a);
-  onlyPR.call(b);
-  onlyBranch.call(c);
-  all.call(a);
-  onlyExecuting.call(d);
-  all.call(a);
-  onlyAborted.call(e);
-  all.call(a);
-  onlyCompleted.call(f);
-  all.call(a);
-  onlyFailed.call(g);
-  all.call(a);
-
-  function addBtn(text, selector, calledfunction) {
+  function addBtn(text, calledfunction) {
     try {
       document.querySelector("#btn" + text).remove();
     } catch (error) {
@@ -45,12 +21,16 @@ function inject() {
     btn.innerText = text;
     btn.setAttribute("id", "btn" + text);
     btn.setAttribute("title", "Click All/PR/Branch first");
-    document.querySelector(selector).append(btn);
+    document.querySelector("nav").append(btn);
     btn.addEventListener("click", calledfunction);
+    calledfunction.call(document.querySelector("#btn" + text));
+    all.call(document.querySelector("#btnAll"));
   }
 
   function all() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -62,7 +42,9 @@ function inject() {
   }
 
   function onlyPR() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -78,7 +60,9 @@ function inject() {
   }
 
   function onlyBranch() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -94,7 +78,9 @@ function inject() {
   }
 
   function onlyExecuting() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -110,7 +96,9 @@ function inject() {
   }
 
   function onlyAborted() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -126,7 +114,9 @@ function inject() {
   }
 
   function onlyCompleted() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -142,7 +132,9 @@ function inject() {
   }
 
   function onlyFailed() {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     for (let i = 0; i < allTableRow.length; i++) {
       let branchName = allTableRow[i].parentNode.parentNode.innerText;
       allTableRow[i].parentNode.parentNode.style.display = "";
@@ -158,7 +150,9 @@ function inject() {
   }
 
   function count(id) {
-    let allTableRow = Array.from(document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap"));
+    let allTableRow = Array.from(
+      document.querySelectorAll(".MuiGrid2-container .MuiGrid2-wrap-xs-nowrap")
+    );
     let sDisplayBlank = 0;
     for (let i = 0; i < allTableRow.length; i++) {
       if (allTableRow[i].parentNode.parentNode.style.display == "") {
